e005e74c436afaedf91eb642cdb0ae83b7214d4c
Shubham Kumar <kshubham5642@gmail.com>
2025-02-28 23:55:07 +0530

    Update pet.js

--- backend/routes/pet.js
+++ backend/routes/pet.js
@@ -13,6 +13,19 @@ const {
 const upload = require("../middleware/multer");
 
 const router = express.Router();
+
+
+router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://pet-adoption-tan-six.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).json({});
+    }
+    next();
+});
 router.get("/api/pet-requests/:userId", getUserPetRequest);
 
 router.get("/api/pet-requests", getAllPetRequests);
